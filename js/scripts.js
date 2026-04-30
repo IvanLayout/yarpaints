ca64bda6ba723bde8a0a12aff7f8a3f09b89b351
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2026-04-30 20:49:44 +0300

    advantages inner

--- js/scripts.js
+++ js/scripts.js
@@ -515,6 +515,50 @@ $(() => {
 			}
 		})
 	}
+
+	if ($('.advantages__slider').length) {
+		new Swiper(".advantages__slider", {
+			loop: false,
+			spaceBetween: 24,
+			slidesPerView: 'auto',
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 15,
+				},
+				'480': {
+					spaceBetween: 15,
+				},
+				'768': {
+					spaceBetween: 15,
+					slidesPerView: 2
+				},
+				'1024': {
+					spaceBetween: 24,
+					slidesPerView: 5
+				}
+			},
+			on: {
+				init: function (swiper) {
+					$(swiper.el).find('.swiper-wrapper').wrap('<div class="swiper-overflow"></div>')
+				}
+			}
+		})
+	}
 });
 
 
