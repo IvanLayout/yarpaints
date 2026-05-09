85076987161fa5613f0489a2afff5ec61abe0ea7
IvanLayuot <ivanpetrachenkov.web@gmail.com>
2026-05-09 17:02:07 +0200

    inspection

--- js/scripts.js
+++ js/scripts.js
@@ -931,6 +931,104 @@ $(() => {
 			}
 		})
 	}
+
+
+	if ($('.stages-work__slider').length) {
+		new Swiper(".stages-work__slider", {
+			loop: false,
+			spaceBetween: 10,
+			slidesPerView: 'auto',
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 10,
+					slidesPerView: 'auto',
+				},
+				'480': {
+					spaceBetween: 10,
+					slidesPerView: 'auto',
+				},
+				'768': {
+					spaceBetween: 20,
+					slidesPerView: 'auto',
+				},
+				'1024': {
+					spaceBetween: 24,
+					slidesPerView: 3
+				},
+				'1200': {
+					spaceBetween: 24,
+					slidesPerView: 4
+				}
+			},
+			on: {
+				init: function (swiper) {
+					$(swiper.el).find('.swiper-wrapper').wrap('<div class="swiper-overflow"></div>')
+				}
+			}
+		})
+	}
+
+
+	if ($('.slider-item').length) {
+		new Swiper(".slider-item", {
+			loop: false,
+			spaceBetween: 10,
+			slidesPerView: 'auto',
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 10,
+					slidesPerView: 'auto',
+				},
+				'480': {
+					spaceBetween: 10,
+					slidesPerView: 'auto',
+				},
+				'768': {
+					spaceBetween: 20,
+					slidesPerView: 'auto',
+				},
+				'1024': {
+					spaceBetween: 24,
+					slidesPerView: 3
+				}
+			},
+			on: {
+				init: function (swiper) {
+					$(swiper.el).find('.swiper-wrapper').wrap('<div class="swiper-overflow"></div>')
+				}
+			}
+		})
+	}
 });
 
 
