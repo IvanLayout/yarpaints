91882e5eb7e69a3540de7ef9747d4618a8e4e114
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2026-05-09 20:25:40 +0300

    product page

--- js/scripts.js
+++ js/scripts.js
@@ -905,6 +905,56 @@ $(() => {
 		})
 	}
 
+	if ($('.products-found__slider').length) {
+		new Swiper(".products-found__slider", {
+			loop: false,
+			spaceBetween: 24,
+			slidesPerView: 'auto',
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 1,
+					slidesPerView: 'auto',
+				},
+				'480': {
+					spaceBetween: 1,
+					slidesPerView: 'auto',
+				},
+				'768': {
+					spaceBetween: 1,
+					slidesPerView: 'auto',
+				},
+				'1024': {
+					spaceBetween: 24,
+					slidesPerView: 5
+				},
+				'1200': {
+					spaceBetween: 24,
+					slidesPerView: 2
+				},
+			},
+			on: {
+				init: function (swiper) {
+					$(swiper.el).find('.swiper-wrapper').wrap('<div class="swiper-overflow"></div>')
+				}
+			}
+		})
+	}
+
 	if ($('.cats-inner__slider').length) {
 		new Swiper(".cats-inner__slider", {
 			loop: false,
